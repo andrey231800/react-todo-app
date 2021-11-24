44ec9ebc012bde3c0661f819485f474fb4a91745
andrey231800 <bambook231800@gmail.com>
2021-11-25 02:14:09 +0300

    react-hooks

---
+++ react-todo-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import ToDoList from './components/TodoList';
+
+function App() {
+  return (
+    <div className="app">
+      <ToDoList />
+    </div>
+  );
+}
+
+export default App;
